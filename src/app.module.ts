16750cac52ce1d51d34344e52bead5f7759fdcb9
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-26 08:49:03 +0900

    [FEAT] postgres Database setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { typeOrmModuleAsyncOptions } from './configs/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.local',
+    }),
+    AuthModule,
+    TypeOrmModule.forRootAsync(typeOrmModuleAsyncOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/database.config.ts
@@ -0,0 +1,19 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const typeOrmModuleAsyncOptions: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+    synchronize: configService.get<boolean>('DB_SYNC'),
+    autoLoadEntities: true,
+    entities: [],
+    logging: false,
+  }),
+};
