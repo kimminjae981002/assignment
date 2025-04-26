6a3832f8f174cb7a54a96a8ee081215de7f6ffff
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-26 23:46:06 +0900

    [UPDATE] 파일 중복 저장 업데이트

--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -4,7 +4,7 @@ import * as ffmpeg from 'fluent-ffmpeg';
 const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 import * as path from 'path';
-
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class VideoService {
   //  음성  추출
@@ -15,7 +15,11 @@ export class VideoService {
     const filePath = path.resolve(file.path); // 업로드된 파일의 절대 경로
 
     // 저장할 파일 경로와 네임
-    const outputAudioPath = path.join(uploadDir, 'audio.mp3');
+    const uniqueFileName = uuidv4();
+    const outputAudioPath = path.join(
+      uploadDir,
+      `${userId}-${uniqueFileName}.mp3`,
+    );
 
     // 음성 추출
     const audio: string = await new Promise((resolve, reject) => {
@@ -37,8 +41,12 @@ export class VideoService {
 
     // 업로드된 파일 경로 (절대 경로로 변환)
     const filePath = path.resolve(file.path); // 업로드된 파일의 절대 경로
+    const uniqueFileName = uuidv4();
 
-    const outputVideoNoAudioPath = path.join(uploadDir, 'video_no_audio.mp4');
+    const outputVideoNoAudioPath = path.join(
+      uploadDir,
+      `${userId}-${uniqueFileName}.mp4`,
+    );
 
     // 영상 오른쪽 이미지 제거 & 음성 제거
     const video: string = await new Promise((resolve, reject) => {
