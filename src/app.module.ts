5aba0b4f1a8b31c471966e2f51cff73cf69075c8
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-26 11:12:03 +0900

    [DOCS] submission 1. submission 폴더 생성 2. submission controller 생성 3. submission service 생성 4. submission module 생성 5. submission entity 생성 6. submission create dto 생성 7. git branch submission

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { typeOrmModuleAsyncOptions } from './configs/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { SubmissionsModule } from './submissions/submissions.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UserModule } from './user/user.module';
     TypeOrmModule.forRootAsync(typeOrmModuleAsyncOptions),
     AuthModule,
     UserModule,
+    SubmissionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
