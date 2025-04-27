faee6ae634a2e0b79f0825aab68bbb386d845375
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-27 19:25:23 +0900

    [FEAT] logger 1. Winston 모듈 다운로드 2. Winston 설정 및 적용 3. git branch logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,8 @@ import { VideoModule } from './video/video.module';
 import { AzureModule } from './azure/azure.module';
 import { RevisionModule } from './revision/revision.module';
 import { StudentModule } from './student/student.module';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
   imports: [
@@ -19,6 +21,22 @@ import { StudentModule } from './student/student.module';
       envFilePath: '.env.local',
     }),
     TypeOrmModule.forRootAsync(typeOrmModuleAsyncOptions),
+    WinstonModule.forRoot({
+      level: 'debug',
+      transports: [
+        new winston.transports.Console({
+          // console에 나오도록 할 것이다. color를 넣고, timestamp 등 내가 원하는 형식
+          format: winston.format.combine(
+            winston.format.colorize({ all: true }),
+            winston.format.timestamp(),
+            winston.format.printf(
+              (info) =>
+                `${info.timestamp} [${info.context}] ${info.level} ${info.message}`,
+            ),
+          ),
+        }),
+      ],
+    }), // debug 이상 모두 로깅하겠다.
     AuthModule,
     StudentModule,
     SubmissionModule,
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as path from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // nest 로거를 winston 기반으로 체인지
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   // 정적 파일 제공 설정 (uploads 폴더)
   app.useStaticAssets(path.join(__dirname, 'uploads'), {
