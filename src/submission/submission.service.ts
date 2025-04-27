1f1e295797ea3ec4999320495609f46825b794b1
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-04-27 09:35:17 +0900

    [CHORE] console.log 삭제

--- src/submission/submission.service.ts
+++ src/submission/submission.service.ts
@@ -64,7 +64,7 @@ export class SubmissionService {
 
     const prompt = `당신은 영어 문법 선생님입니다. 내용에 대하여 score, feedback, highlights를 작성해주세요. 내용: ${submitText} 답변은 한국어로 부탁드리고 답변 예시 형식에 꼭 맞춰주시고 JSON 형식으로 반환해주세요.
     답변 예시) score: 2 10점 만점 평가, feedback: 전반적으로 잘 작성했지만 부족한 부분을 말씀 드리겠습니다, highlights: ["test", "where"] 내용에 대해 감전한 부분을 배열로 넣기, highlightSubmitText: 내용에 highlights에 속한 게 있다면 내용에 <b>test</b> 이런 식으로 반환해줘 내용에 넣어서 `;
-    console.log('tes1t');
+
     // 영상 & 음성 추출
     // Azure에 비디오 & 오디오 추출 파일 저장
     if (videoFile) {
@@ -87,10 +87,10 @@ export class SubmissionService {
         'video',
       );
     }
-    console.log('test', videoFile);
+
     // ai 답변 가져오기
     const aiAnswer = await this.azureOpenAIService.openAI(prompt);
-    console.log('tes2t', videoFile);
+
     // submission DB에 저장
     const submission = this.submissionRepository.create({
       ...createSubmissionDto,
